be991dd2de5caddb4c1c10a0753f03af093a4907
oney erge <oneyerge@gmail.com>
2020-07-15 23:51:27 -0500

    Fixed the chart. Data can be plotted at Apexcharts now. Fixed other minor pieces.

--- server/server.py
+++ server/server.py
@@ -55,34 +55,14 @@ def get_form_data(d):
 
     print("here is the passed data")
     print(projection_date, lookback, stock)
-    ##ToDo do some calculation pass the result to the client
     trained_model_path = 'trained models/EOG.h5'
-
     historical_data_path = 'Data/EOG.csv'
-
     df = pd.read_csv(historical_data_path, usecols=['Date', 'Close'])
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
-
     df = predict_stock_price(df, projection_date, trained_model_path, lookback)
     df.index = df.index.astype(str)
-    #values = df.values.tolist()
-
-
-
-
-
     socketio.emit('plot_data_from_server', {'data': df.to_json(orient='index')})
-    #print('tried emitting following data: {}'.format(df.to_json()))
-
-
-
-        # jsonfiles = json.loads(json.dumps(dict_to_update))
-        # return jsonify(jsonfiles)
-
-#def emit_plot_data():
-#    data = 123  # preparing the data in dictionary format maybe needed to convert to json, I dont know yet.
-#    socketio.emit('plot_data_from_server', data)
 
 
 if __name__ == ' __main__':
